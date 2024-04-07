34de2c39f4e57055439f778d4241bc27685ba6e0
Him <williamliu263@gmail.com>
2024-04-08 01:45:09 +0800

    Update

--- app/src/main/java/com/example/sehh3165_gp/Lobby.java
+++ app/src/main/java/com/example/sehh3165_gp/Lobby.java
@@ -1,4 +1,12 @@
 package com.example.sehh3165_gp;
 
-public class Lobby {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lobby extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lobby);
+    }
 }

dc48cb6ca73b26c24441a26160fdb96ae902004d
Him <williamliu263@gmail.com>
2024-04-17 10:03:32 +0800

    Update

--- app/src/main/java/com/example/sehh3165_gp/Game2.java
+++ app/src/main/java/com/example/sehh3165_gp/Game2.java
@@ -1,8 +1,11 @@
 package com.example.sehh3165_gp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -14,41 +17,72 @@ public class Game2 extends AppCompatActivity implements View.OnClickListener, Vi
 
     float xAxis, yAxis, buttonX, buttonY;
     int lastAction;
-    ImageButton home, sound, game_hint, game_reset, next;
-    ImageButton cryingBaby, toy, toy2, toy3;
-    String email;
+
+    ImageButton home, sound, mute, game_hint, game_reset;
+    ImageButton cryingBaby, smilingBaby, toy, toy2, toy3;
+
     SharedPreferences prefs;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game2_baby);
-
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
         Bundle extras = getIntent().getExtras();
         email = extras != null ? extras.getString("Email") : null;
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
         home = findViewById(R.id.imageButton_home);
         sound = findViewById(R.id.imageButton_sound);
+        mute = findViewById(R.id.imageButton_mute);
         game_hint = findViewById(R.id.imageButton_hint);
         game_reset = findViewById(R.id.imageButton_reset);
-        next = findViewById(R.id.imageButton_next);
-
         home.setOnClickListener(this);
         sound.setOnClickListener(this);
+        mute.setOnClickListener(this);
         game_hint.setOnClickListener(this);
         game_reset.setOnClickListener(this);
-        next.setOnClickListener(this);
 
         cryingBaby = findViewById(R.id.imageButton_crying_baby);
+        smilingBaby = findViewById(R.id.imageButton_stop_crying_baby);
         toy = findViewById(R.id.imageButton_toy);
         toy2 = findViewById(R.id.imageButton_toy2);
         toy3 = findViewById(R.id.imageButton_toy3);
-
         toy.setOnTouchListener(this);
         toy2.setOnTouchListener(this);
         toy3.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                buttonX = v.getX();
+                buttonY = v.getY();
+                xAxis = buttonX - event.getRawX();
+                yAxis = buttonY - event.getRawY();
+                lastAction = MotionEvent.ACTION_DOWN;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                v.setX(event.getRawX() + xAxis);
+                v.setY(event.getRawY() + yAxis);
+                lastAction = MotionEvent.ACTION_MOVE;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (lastAction == MotionEvent.ACTION_MOVE) {
+                    if (Overlapped(v, cryingBaby)) {
+                        Toast.makeText(this, "X", Toast.LENGTH_SHORT).show();
+                    }
+                    v.performClick();
+                    v.setX(buttonX);
+                    v.setY(buttonY);
+                }
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 
     @Override
@@ -111,50 +145,34 @@ public class Game2 extends AppCompatActivity implements View.OnClickListener, Vi
         }
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_DOWN:
-                buttonX = v.getX();
-                buttonY = v.getY();
-                xAxis = buttonX - event.getRawX();
-                yAxis = buttonY - event.getRawY();
-                lastAction = MotionEvent.ACTION_DOWN;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                v.setX(event.getRawX() + xAxis);
-                v.setY(event.getRawY() + yAxis);
-                lastAction = MotionEvent.ACTION_MOVE;
-                break;
-            case MotionEvent.ACTION_UP:
-                if (lastAction == MotionEvent.ACTION_MOVE) {
-                    if (Overlapped(v, cryingBaby)) {
-                        Toast.makeText(this, "X", Toast.LENGTH_SHORT).show();
-                    }
-                    v.performClick();
-                    v.setX(buttonX);
-                    v.setY(buttonY);
-                }
-                break;
-            default:
-                return false;
-        }
-        return true;
-    }
 
     private boolean Overlapped(View firstView, View secondView) {
         int[] firstPosition = new int[2];
         int[] secondPosition = new int[2];
-
         firstView.getLocationOnScreen(firstPosition);
         secondView.getLocationOnScreen(secondPosition);
-
         int firstViewRight = firstPosition[0] + firstView.getWidth();
         int firstViewBottom = firstPosition[1] + firstView.getHeight();
         int secondViewRight = secondPosition[0] + secondView.getWidth();
         int secondViewBottom = secondPosition[1] + secondView.getHeight();
-
         return !(firstPosition[0] > secondViewRight || firstViewRight < secondPosition[0] ||
                 firstPosition[1] > secondViewBottom || firstViewBottom < secondPosition[1]);
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        boolean isMusicMute = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) == 0;
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && isMusicMute) {
+            win();
+            return super.onKeyDown(keyCode, event);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void win() {
+        Toast.makeText(this,  "v", Toast.LENGTH_SHORT).show();
+        cryingBaby.setVisibility(View.INVISIBLE);
+        smilingBaby.setVisibility(View.VISIBLE);
+    }
+
 }
